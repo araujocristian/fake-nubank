692a945a3ae13743e1e863e3cc4fb9f0c8bb792e
Cristian Araujo <araujocristian@live.com>
2020-02-01 18:00:32 -0300

    Criados Header e Tab

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, TabsContainer, TabItem, TabText } from './styles';
+
+export default function Tabs() {
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
+}
