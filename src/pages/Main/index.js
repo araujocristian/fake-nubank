95d207dd3fc7589d7e693d2be1df76c1099dd04b
Cristian Araujo <araujocristian@live.com>
2020-02-01 18:57:58 -0300

    Add novas tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from '~/components/Header';
 import { Container } from './styles';
+import Tabs from '~/components/Tabs';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Tabs />
     </Container>
   );
 }
