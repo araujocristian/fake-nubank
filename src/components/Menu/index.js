74ecd309f5c106be7512875b685cb600b53f15f0
Cristian Araujo <araujocristian@live.com>
2020-02-01 23:12:37 -0300

    Finalizado Menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Code } from './styles';
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavItemInit,
+  NavText,
+  SingOutButton,
+  SingOutButtonText,
+} from './styles';
 
 export default function Menu() {
   return (
@@ -15,6 +25,40 @@ export default function Menu() {
           color="#8b10ae"
         />
       </Code>
+      <Nav>
+        <NavItem>
+          <NavItemInit>
+            <Icon name="help-outline" size={20} color="#fff" />
+            <NavText>Me ajuda</NavText>
+          </NavItemInit>
+          <Icon name="keyboard-arrow-right" size={20} color="#fff" />
+        </NavItem>
+        <NavItem>
+          <NavItemInit>
+            <Icon name="person-outline" size={20} color="#fff" />
+            <NavText>Perfil</NavText>
+          </NavItemInit>
+          <Icon name="keyboard-arrow-right" size={20} color="#fff" />
+        </NavItem>
+        <NavItem>
+          <NavItemInit>
+            <Icon name="credit-card" size={20} color="#fff" />
+            <NavText>Configurar cartão</NavText>
+          </NavItemInit>
+          <Icon name="keyboard-arrow-right" size={20} color="#fff" />
+        </NavItem>
+        <NavItem>
+          <NavItemInit>
+            <Icon name="smartphone" size={20} color="#fff" />
+            <NavText>Configurações do app</NavText>
+          </NavItemInit>
+          <Icon name="keyboard-arrow-right" size={20} color="#fff" />
+        </NavItem>
+      </Nav>
+
+      <SingOutButton onPress={() => {}}>
+        <SingOutButtonText>SAIR DO APP</SingOutButtonText>
+      </SingOutButton>
     </Container>
   );
 }
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,5 +1,56 @@
+import { StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin: 0 30px;
+`;
 
-export const Code = styled.View``;
+export const Code = styled.View`
+  background: #fff;
+  padding: 5px;
+  align-self: center;
+`;
+
+export const Nav = styled.View`
+  margin-top: 10px;
+  align-self: stretch;
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  border-top-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavItem = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 0;
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-bottom-color: rgba(255, 255, 255, 0.8);
+`;
+export const NavItemInit = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const NavText = styled.Text`
+  font-size: 15px;
+  color: #fff;
+  margin-left: 20px;
+`;
+
+export const SingOutButton = styled.TouchableOpacity`
+  border-width: ${StyleSheet.hairlineWidth}px;
+  border-color: rgba(255, 255, 255, 0.8);
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  margin-top: 15px;
+`;
+
+export const SingOutButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 13px;
+`;
